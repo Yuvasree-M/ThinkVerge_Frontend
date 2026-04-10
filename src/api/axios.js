a6ca3b21e3f395b13a4c5f36f3dc0ecd7c434f69
Yuvasree-M <yuvasreemohan4sep2003@gmail.com>
2026-04-10 11:42:01 +0530

    Deployed

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,10 +1,9 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api', // backend port
+  baseURL: process.env.REACT_APP_BASE_URL,
   headers: { 'Content-Type': 'application/json' },
 })
-
 // Attach JWT from localStorage
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token')
